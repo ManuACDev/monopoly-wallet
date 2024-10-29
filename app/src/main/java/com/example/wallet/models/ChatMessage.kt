3b053a94b282456ddd805b176c3448ad04cab95a
ManuACDev <malamocres@gmail.com>
2024-10-29 18:01:39 +0100

    ChatMessage Model Type

--- app/src/main/java/com/example/wallet/models/ChatMessage.kt
+++ app/src/main/java/com/example/wallet/models/ChatMessage.kt
@@ -2,5 +2,6 @@ package com.example.wallet.models
 
 data class ChatMessage(
     val name: String, // Nombre del remitente
-    val content: String // Contenido del mensaje
+    val content: String, // Contenido del mensaje
+    val type: String // Tipo del mensaje
 )
